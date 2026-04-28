cde55fb6d856a575f8651c10245a0f7845200377
Almirodesouza <0almiro0de0souza0@gmail.com>
2026-04-28 19:36:45 -0300

    second-Commit

--- scripts.js
+++ scripts.js
@@ -2,12 +2,23 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
+    const moneyReal = document.querySelector(".money-Real")
+    const moneyConverted = document.querySelector(".money-Dolar")
 
     const dolarToday = 5.0
 
     const convertedValue = inputCurrencyValue / dolarToday
 
-    console.log(convertedValue)
+    moneyReal.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    moneyConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
 }
 
 convertButton.addEventListener("click", convertValues)
